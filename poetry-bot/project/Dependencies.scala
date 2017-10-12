b7b0d814cdf0c5a8f6cf9ec5a5aa8fdbe591aa16
Yuriy <basov@restream.rt.ru>
2017-10-12 18:20:56 +0300

    final commit

--- poetry-bot/project/Dependencies.scala
+++ poetry-bot/project/Dependencies.scala
@@ -1,41 +1,43 @@
-import Dependencies._
+import sbt._
 
-lazy val general = Seq(
-  name := "poetry-bot",
-  organization := "io.github.username",
-  version := "0.0.1",
-  scalaVersion := "2.12.1"
-)
+object Dependencies {
+  lazy val catsV          = "0.9.0"
+  lazy val akkaHttpV      = "10.0.5"
+  lazy val circeV         = "0.7.0"
+  lazy val ficusV         = "1.4.0"
+  lazy val scalaMockV     = "3.5.0"
+  lazy val catsScalatestV = "2.2.0"
+  // EXTRA
+  lazy val loggingV       = "3.7.2"
+  lazy val telegramV      = "3.2"
 
-lazy val dependencies = Seq(
-  pack1,
-  pack2,
-  pack3,
-  pack4,
-  pack5,
-  pack6,
-  pack7,
-  pack8 % "test",
-  pack9 % "test",
-  pack10 % "test",
-  pack11,
-  pack12,
-  pack13,
-  pack14
-)
+  lazy val pack1       = "org.typelevel"     %% "cats-core"       % catsV
+  lazy val pack2       = "com.iheart"        %% "ficus"           % ficusV
+  lazy val pack3       = "com.typesafe.akka" %% "akka-http"       % akkaHttpV
+  lazy val pack4       = "de.heikoseeberger" %% "akka-http-circe" % "1.15.0"
+  lazy val pack5       = "io.circe"          %% "circe-core"      % circeV
+  lazy val pack6       = "io.circe"          %% "circe-generic"   % circeV
+  lazy val pack7       = "io.circe"          %% "circe-parser"    % circeV
+  lazy val pack8       = "org.scalamock"     %% "scalamock-scalatest-support" % scalaMockV
+  lazy val pack9       = "com.ironcorelabs"  %% "cats-scalatest"              % catsScalatestV
+  lazy val pack10      = "com.typesafe.akka" %% "akka-http-testkit"           % akkaHttpV
+  // EXTRA
+  lazy val pack11      = "com.typesafe.scala-logging" %% "scala-logging"      % loggingV
+  lazy val pack12      = "org.telegram" % "telegrambots"                     % telegramV
 
-lazy val root = (project in file("."))
-  .settings(
-    general,
-    assemblyMergeStrategy in assembly := {
-      case PathList("META-INF", xs @ _*) => MergeStrategy.discard
-      case x => MergeStrategy.first
-    },
-    resolvers ++= repos,
-    libraryDependencies ++= dependencies,
-    // dependencyOverrides ++= overrides,
-    assemblyJarName in assembly := name.value + "-" + version.value + ".jar"
+  lazy val pack13      = "com.typesafe" % "config" % "1.3.1"
+  lazy val pack14      = "org.slf4j" % "slf4j-jdk14" % "1.7.25"
+
+  lazy val repos = Seq(Resolver.jcenterRepo)
+
+  lazy val overrides = Seq(
+    "commons-net" % "commons-net" % "3.1",
+    "com.google.guava" % "guava" % "14.0.1",
+    "com.google.code.findbugs" % "jsr305" % "3.0.0",
+    "io.netty" % "netty" % "3.8.0.Final",
+    "io.netty" % "netty-all" % "4.0.29.Final"
   )
+}
 
 //name := "poetry-bot"
 //organization := "io.github.username"
@@ -77,6 +79,7 @@ lazy val root = (project in file("."))
 //Defaults.itSettings
 //Revolver.settings
 //enablePlugins(JavaAppPackaging)
+//
 //
 //initialCommands := """
 //import cats._
--- poetry-bot/project/plugins.sbt
+++ poetry-bot/project/plugins.sbt
@@ -1,7 +1,9 @@
-resolvers += Classpaths.sbtPluginReleases
+// resolvers += Classpaths.sbtPluginReleases
 
-addSbtPlugin("com.geirsson" % "sbt-scalafmt" % "0.5.5")
-addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.8.0")
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.0")
-addSbtPlugin("io.spray" %% "sbt-revolver" % "0.8.0")
-addSbtPlugin("com.typesafe.sbt" %% "sbt-native-packager" % "1.2.0-M8")
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.5")
+
+// addSbtPlugin("com.geirsson" % "sbt-scalafmt" % "0.5.5")
+// addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.8.0")
+// addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.0")
+// addSbtPlugin("io.spray" %% "sbt-revolver" % "0.8.0")
+// addSbtPlugin("com.typesafe.sbt" %% "sbt-native-packager" % "1.2.0-M8")
